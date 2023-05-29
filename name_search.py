d306c0db4882f2f8b828d4a69c23e57d545dacfe
ayushhsahu <133361991+ayushhsahu@users.noreply.github.com>
2023-05-29 08:36:14 +0530

    Create name_search.py

---
+++ name_search.py
@@ -0,0 +1,7 @@
+names = ["satyam", "shubham", "aarchi", "vishal", "vinay", "niketa"]
+name = input("Enter the name to check\n")
+
+if name in names:
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
